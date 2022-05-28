feffc3fea11b1677b70f36824578a6e73937be37
skiriel <gabrieldllopes@gmail.com>
2022-05-28 12:31:17 -0300

    Instalando arquivos iniciais

---
+++ intro/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+const App = () => {
+  return (
+  <div className='App'>
+    Hello World
+  </div>
+  )
+}
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(<App />, rootElement);
